b2a2f3368bc578b6e94580f4fba9926bb86254ba
Sy Lim <slimdoe2@gmail.com>
2019-02-26 14:06:13 -0800

    updated

---
+++ assets/javascript/game.js
@@ -0,0 +1,22 @@
+// started my js:
+
+var thumbUp=0;
+var thumbDown=0;
+var wins=0;
+var losses=0;
+// i added function here:
+function guessWhat(result) {
+var randomGuess = Math.floor(Math.random()*2);
+
+    
+}
+
+
+
+
+
+
+
+
+
+
